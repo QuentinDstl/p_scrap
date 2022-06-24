9a383772bf947d99755da6313bd59f4d8db80be6
QuentinDstl <qdesautel@gmail.com>
2022-06-24 14:01:55 +0530

    modif init

--- init.py
+++ init.py
@@ -1,15 +1,20 @@
-import os
+from os.path import dirname as OsDirname, abspath as Osabspath, join as OsJoin
+from os import system as OsSystem, getenv as OsGetenv
+# loading the environment variables
+from dotenv import load_dotenv
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+load_dotenv()
+
+ROOT_DIR = OsDirname(Osabspath(__file__))  # Project Root
 # directory of the user for the chrome driver
-DIR_CHROMEPROFIL_PATH = os.path.join(ROOT_DIR, r'driver\\driverProfile\\')
-# TODO faire un installeur pour pouvoir save les variables global suivante dans un fichier
-# directory of the chrome application where chrome.exe is located
-DIR_CHROMEAPP_PATH = r'C:\\Program Files\\Google\\Chrome\\Application\\'
-# port to use for the chrome debugger
-PORT = 9222
+DIR_CHROMEPROFIL_PATH = OsJoin(ROOT_DIR, r'driver\\driverProfile\\')
 
-# os.system(
-#     'cmd /k "set PATH=%PATH%;%s"' % DIR_CHROMEAPP_PATH)
-os.system(
-    'cmd /k chrome.exe --remote-debugging-port=%d --user-data-dir=%s' % (PORT, DIR_CHROMEPROFIL_PATH))
+
+# def initChromeWindow():
+#     OsSystem(
+#         "cmd /k set PATH=%PATH%;%s" % OsGetenv('DIR_CHROMEAPP_PATH'))
+#     OsSystem(
+#         'cmd /k chrome.exe --remote-debugging-port=%d --user-data-dir=%s' % (OsGetenv('PORT'), DIR_CHROMEPROFIL_PATH))
+
+OsSystem(
+    'cmd /k chrome.exe --remote-debugging-port=%d --user-data-dir=%s' % (OsGetenv('PORT'), DIR_CHROMEPROFIL_PATH))
