6d667fdc1376bd3519ea209c7c9929dcd235038e
QuentinDstl <qdesautel@gmail.com>
2022-06-27 19:38:36 +0530

    modif message de base

--- parcs.py
+++ parcs.py
@@ -297,7 +297,7 @@ def main(driver):
     cls_button.place(x=376.0, y=162.0, width=19.0, height=19.0)
 
     window.resizable(False, False)
-    guiPrint(label, "This is a scrollable window to display error messages. To clean up messages you have to click on the cross")
+    guiPrint(label, "This is a scrollable window to display error messages. To clean up messages click on the cross ->")
                     
 
     def onClosing():
